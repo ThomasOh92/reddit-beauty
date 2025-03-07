c5d247bdaf07efe84a7f08600f318fe34cc53a98
Thomas Oh <ohcsthomas@gmail.com>
2025-03-07 21:16:11 +0000

    change to next.config, images

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'kyliejennercosmetics.co.uk',
+        port: '',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
