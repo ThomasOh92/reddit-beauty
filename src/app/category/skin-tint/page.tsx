fb9e5dd67dd448ad88be1d57cb6dc343cf69a0c4
Thomas Oh <ohcsthomas@gmail.com>
2025-03-08 13:12:44 +0000

    initial work on skin tint page

--- src/app/category/skin-tint/page.tsx
+++ src/app/category/skin-tint/page.tsx
@@ -1,9 +1,15 @@
-  
+import ProductsTable from "@/components/productstable"
+
 export default function SkinTint() {
 
       return (
-        <h1>Skin Tint Page</h1>
-      )
+        <div className="flex flex-col items-center min-h-screen bg-gray-100">
+          <h1 className="text-4xl font-bold mb-8">Skin Tint</h1>
+          <div className="flex flex-wrap justify-center space-x-4">
+            <ProductsTable />
+          </div>
+        </div>
+    )
 }
 
   
