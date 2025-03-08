38d8a0d2de0020ebb19faab1a3ac3eea1bd1f7d2
Thomas Oh <ohcsthomas@gmail.com>
2025-03-08 21:33:30 +0000

    slightly more progress on the skin tint page

--- src/app/category/skin-tint/page.tsx
+++ src/app/category/skin-tint/page.tsx
@@ -4,10 +4,15 @@ export default function SkinTint() {
 
       return (
         <div className="flex flex-col items-center min-h-screen bg-gray-100">
-          <h1 className="text-4xl font-bold mb-8">Skin Tint</h1>
-          <div className="flex flex-wrap justify-center space-x-4">
+          <h1 className="text-4xl font-semibold mb-8 mt-4 bg-clip-text">
+              Top Reddit Choices: <span className="bg-gradient-to-r from-red-400 to-pink-500 text-transparent bg-clip-text">Skin Tint</span>
+          </h1>
+          <div className="flex flex-wrap justify-center space-x-4 w-full">
+            <p className="text-2xl font-bold mr-10 self-center">Top 5 <br/>Products</p>
             <ProductsTable />
           </div>
+            <div className="divider w-full max-w-4xl mx-auto"></div>
+
         </div>
     )
 }
--- src/components/productstable.tsx
+++ src/components/productstable.tsx
@@ -15,139 +15,137 @@ const ProductsTable: React.FC = () => {
             </tr>
             </thead>
             <tbody>
-            {/* row 1 */}
-            <tr>
-                <th>1</th>
-                <td>
-                <div className="flex items-center gap-3">
-                    <div className="avatar">
-                    <div className="mask mask-squircle h-12 w-12">
-                        <img
-                        src="https://img.daisyui.com/images/profile/demo/2@94.webp"
-                        alt="Avatar Tailwind CSS Component" />
+                {/* row 1 */}
+                <tr>
+                    <th>1</th>
+                    <td>
+                    <div className="flex items-center gap-3">
+                        <div className="avatar">
+                        <div className="mask mask-squircle h-12 w-12">
+                            <img
+                            src="https://img.daisyui.com/images/profile/demo/2@94.webp"
+                            alt="Avatar Tailwind CSS Component" />
+                        </div>
+                        </div>
+                        <div>
+                        <div className="font-bold">Product Name</div>
+                        </div>
                     </div>
+                    </td>
+                    <td>
+                    <span className="text-sm"> Description</span>
+                    <br />
+                    </td>
+                    <td><span className="text-xs opacity-500">r/MakeupAddictionUK ...................................................</span></td>
+                    <th>
+                    <button className="btn btn-ghost btn-xs">details</button>
+                    </th>
+                </tr>
+                {/* row 2 */}
+                <tr>
+                    <th>2</th>
+                    <td>
+                    <div className="flex items-center gap-3">
+                        <div className="avatar">
+                        <div className="mask mask-squircle h-12 w-12">
+                            <img
+                            src="https://img.daisyui.com/images/profile/demo/2@94.webp"
+                            alt="Avatar Tailwind CSS Component" />
+                        </div>
+                        </div>
+                        <div>
+                        <div className="font-bold">Product Name</div>
+                        </div>
                     </div>
-                    <div>
-                    <div className="font-bold">Product Name</div>
+                    </td>
+                    <td>
+                    <span className="text-sm"> Description</span>
+                    <br />
+                    </td>
+                    <td><span className="text-xs opacity-500">r/MakeupAddictionUK ....</span></td>
+                    <th>
+                    <button className="btn btn-ghost btn-xs">details</button>
+                    </th>
+                </tr>
+                {/* row 3 */}
+                <tr>
+                    <th>3</th>
+                    <td>
+                    <div className="flex items-center gap-3">
+                        <div className="avatar">
+                        <div className="mask mask-squircle h-12 w-12">
+                            <img
+                            src="https://img.daisyui.com/images/profile/demo/2@94.webp"
+                            alt="Avatar Tailwind CSS Component" />
+                        </div>
+                        </div>
+                        <div>
+                        <div className="font-bold">Product Name</div>
+                        </div>
                     </div>
-                </div>
-                </td>
-                <td>
-                <span className="text-sm"> Description</span>
-                <br />
-                </td>
-                <td><span className="text-xs opacity-500">r/MakeupAddictionUK ....</span></td>
-                <th>
-                <button className="btn btn-ghost btn-xs">details</button>
-                </th>
-            </tr>
-            {/* row 2 */}
-            <tr>
-                <th>
-                <label>
-                    <input type="checkbox" className="checkbox" />
-                </label>
-                </th>
-                <td>
-                <div className="flex items-center gap-3">
-                    <div className="avatar">
-                    <div className="mask mask-squircle h-12 w-12">
-                        <img
-                        src="https://img.daisyui.com/images/profile/demo/3@94.webp"
-                        alt="Avatar Tailwind CSS Component" />
+                    </td>
+                    <td>
+                    <span className="text-sm"> Description</span>
+                    <br />
+                    </td>
+                    <td><span className="text-xs opacity-500">r/MakeupAddictionUK ....</span></td>
+                    <th>
+                    <button className="btn btn-ghost btn-xs">details</button>
+                    </th>
+                </tr>
+                {/* row 4 */}
+                <tr>
+                    <th>4</th>
+                    <td>
+                    <div className="flex items-center gap-3">
+                        <div className="avatar">
+                        <div className="mask mask-squircle h-12 w-12">
+                            <img
+                            src="https://img.daisyui.com/images/profile/demo/2@94.webp"
+                            alt="Avatar Tailwind CSS Component" />
+                        </div>
+                        </div>
+                        <div>
+                        <div className="font-bold">Product Name</div>
+                        </div>
                     </div>
+                    </td>
+                    <td>
+                    <span className="text-sm"> Description</span>
+                    <br />
+                    </td>
+                    <td><span className="text-xs opacity-500">r/MakeupAddictionUK ....</span></td>
+                    <th>
+                    <button className="btn btn-ghost btn-xs">details</button>
+                    </th>
+                </tr>
+                {/* row 5 */}
+                <tr>
+                    <th>5</th>
+                    <td>
+                    <div className="flex items-center gap-3">
+                        <div className="avatar">
+                        <div className="mask mask-squircle h-12 w-12">
+                            <img
+                            src="https://img.daisyui.com/images/profile/demo/2@94.webp"
+                            alt="Avatar Tailwind CSS Component" />
+                        </div>
+                        </div>
+                        <div>
+                        <div className="font-bold">Product Name</div>
+                        </div>
                     </div>
-                    <div>
-                    <div className="font-bold">Brice Swyre</div>
-                    <div className="text-sm opacity-50">China</div>
-                    </div>
-                </div>
-                </td>
-                <td>
-                Carroll Group
-                <br />
-                <span className="badge badge-ghost badge-sm">Tax Accountant</span>
-                </td>
-                <td>Red</td>
-                <th>
-                <button className="btn btn-ghost btn-xs">details</button>
-                </th>
-            </tr>
-            {/* row 3 */}
-            <tr>
-                <th>
-                <label>
-                    <input type="checkbox" className="checkbox" />
-                </label>
-                </th>
-                <td>
-                <div className="flex items-center gap-3">
-                    <div className="avatar">
-                    <div className="mask mask-squircle h-12 w-12">
-                        <img
-                        src="https://img.daisyui.com/images/profile/demo/4@94.webp"
-                        alt="Avatar Tailwind CSS Component" />
-                    </div>
-                    </div>
-                    <div>
-                    <div className="font-bold">Marjy Ferencz</div>
-                    <div className="text-sm opacity-50">Russia</div>
-                    </div>
-                </div>
-                </td>
-                <td>
-                Rowe-Schoen
-                <br />
-                <span className="badge badge-ghost badge-sm">Office Assistant I</span>
-                </td>
-                <td>Crimson</td>
-                <th>
-                <button className="btn btn-ghost btn-xs">details</button>
-                </th>
-            </tr>
-            {/* row 4 */}
-            <tr>
-                <th>
-                <label>
-                    <input type="checkbox" className="checkbox" />
-                </label>
-                </th>
-                <td>
-                <div className="flex items-center gap-3">
-                    <div className="avatar">
-                    <div className="mask mask-squircle h-12 w-12">
-                        <img
-                        src="https://img.daisyui.com/images/profile/demo/5@94.webp"
-                        alt="Avatar Tailwind CSS Component" />
-                    </div>
-                    </div>
-                    <div>
-                    <div className="font-bold">Yancy Tear</div>
-                    <div className="text-sm opacity-50">Brazil</div>
-                    </div>
-                </div>
-                </td>
-                <td>
-                Wyman-Ledner
-                <br />
-                <span className="badge badge-ghost badge-sm">Community Outreach Specialist</span>
-                </td>
-                <td>Indigo</td>
-                <th>
-                <button className="btn btn-ghost btn-xs">details</button>
-                </th>
-            </tr>
+                    </td>
+                    <td>
+                    <span className="text-sm"> Description</span>
+                    <br />
+                    </td>
+                    <td><span className="text-xs opacity-500">r/MakeupAddictionUK ....</span></td>
+                    <th>
+                    <button className="btn btn-ghost btn-xs">details</button>
+                    </th>
+                </tr>
             </tbody>
-            {/* foot */}
-            <tfoot>
-            <tr>
-                <th></th>
-                <th>Name</th>
-                <th>Job</th>
-                <th>Favorite Color</th>
-                <th></th>
-            </tr>
-            </tfoot>
         </table>
         </div>    
 
