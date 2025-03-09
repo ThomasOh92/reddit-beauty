20be61b84111172bf1bbfdbaf5cfff1f0c1363f8
Thomas Oh <ohcsthomas@gmail.com>
2025-03-09 10:44:31 +0000

    more progress on skin tint page

--- src/app/category/skin-tint/page.tsx
+++ src/app/category/skin-tint/page.tsx
@@ -1,18 +1,30 @@
-import ProductsTable from "@/components/productstable"
+import ProductCard from "@/components/productcard"
 
 export default function SkinTint() {
 
       return (
-        <div className="flex flex-col items-center min-h-screen bg-gray-100">
-          <h1 className="text-4xl font-semibold mb-8 mt-4 bg-clip-text">
-              Top Reddit Choices: <span className="bg-gradient-to-r from-red-400 to-pink-500 text-transparent bg-clip-text">Skin Tint</span>
-          </h1>
-          <div className="flex flex-wrap justify-center space-x-4 w-full">
-            <p className="text-2xl font-bold mr-10 self-center">Top 5 <br/>Products</p>
-            <ProductsTable />
-          </div>
-            <div className="divider w-full max-w-4xl mx-auto"></div>
-
+        <div className="flex flex-row min-h-screen bg-gray-100">
+              {/* Left section */}
+              <div className="text-2xl mb-8 mt-8 ml-4 bg-clip-text font-semibold">
+                <p>Discussions Analyzed</p>
+              </div>
+               {/* Divider */}
+              <div className="divider divider-horizontal"></div>
+              {/* Right section */}
+                <div className="w-3/4 p-4 flex flex-wrap">
+                <h1 className="text-4xl font-semibold mb-8 mt-4 w-full bg-clip-text">
+                Reddit Most Talked About: <span className="bg-gradient-to-r from-red-400 to-pink-500 text-transparent bg-clip-text">Skin Tint</span>
+                </h1>
+                <div className="flex flex-wrap">
+                  <ProductCard />
+                  <ProductCard />
+                  <ProductCard />
+                  <ProductCard />
+                  <ProductCard />
+                  <ProductCard />
+                  <ProductCard />
+                </div>
+                </div>
         </div>
     )
 }
