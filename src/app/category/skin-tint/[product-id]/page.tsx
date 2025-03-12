ddc9271eb57f87683324a69eac2c696eab60b405
Thomas Oh <ohcsthomas@gmail.com>
2025-03-12 17:51:17 +0000

    redeploy page - fix product page

--- src/app/category/skin-tint/[product-id]/page.tsx
+++ src/app/category/skin-tint/[product-id]/page.tsx
@@ -1,13 +1,18 @@
 import Link from 'next/link';
 
-export default function ProductPage({ params }: { params: { productId: string } }) {
-  
+interface ProductPageProps {
+  params: Promise<{ 'product-id': string }>
+}
+
+export default async function ProductPage({ params }: ProductPageProps) {
+  const resolvedParams = await params;
   return (
     <div className="p-8">
-      <h1>Product ID: {params.productId}</h1>
+      <h1>Product ID: {resolvedParams['product-id']}</h1>
       <h1 className="text-4xl font-bold">Product Name</h1>
       <p className="mt-4 text-lg">Product Description</p>
       <Link href="/category/skin-tint" className="mt-6 text-blue-500">Go Back</Link>
     </div>
   );
 }
+
