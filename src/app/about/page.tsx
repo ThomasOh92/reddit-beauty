8036fa17811b01e2335387b3e8f34b53a0001abe
Thomas Oh <ohcsthomas@gmail.com>
2025-03-15 21:07:32 +0000

    deployment fix for text on about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -13,7 +13,7 @@ export default function AboutPage() {
             <div className="bg-base-100 p-6 shadow-lg w-full border-base-300 border rounded-none">
                 <h2 className="text-l font-semibold">Our Mission</h2>
                 <p className="mt-4 text-sm">
-                    Our mission is to empower beauty enthusiasts by providing insights from Reddit's vibrant communities, offering reliable and data-driven recommendations.
+                    Our mission is to empower beauty enthusiasts by providing insights from Reddit&apos;s vibrant communities, offering reliable and data-driven recommendations.
                 </p>
             </div>
 
@@ -31,7 +31,7 @@ export default function AboutPage() {
             <div className="bg-base-100 p-6 shadow-lg w-full border-base-300 border rounded-none">
                 <h2 className="text-l font-semibold">Meet Our Team</h2>
                 <p className="mt-4 text-sm">
-                    Our team consists of data analysts, beauty enthusiasts, and tech professionals who are passionate about uncovering the best beauty insights from Reddit's communities.
+                    Our team consists of data analysts, beauty enthusiasts, and tech professionals who are passionate about uncovering the best beauty insights from Reddit&apos;s communities.
                 </p>
             </div>
 
@@ -39,7 +39,7 @@ export default function AboutPage() {
             <div className="bg-base-100 p-6 shadow-lg w-full border-base-300 border rounded-none">
                 <h2 className="text-l font-semibold">Get in Touch</h2>
                 <p className="mt-4 text-sm">
-                    Have questions or feedback? Feel free to reach out to us at <a href="mailto:contact@beautyplatform.com" className="text-blue-500 underline">contact@beautyplatform.com</a>. We'd love to hear your thoughts!
+                    Have questions or feedback? Feel free to reach out to us at <a href="mailto:contact@beautyplatform.com" className="text-blue-500 underline">contact@beautyplatform.com</a>. We&apos;d love to hear your thoughts!
                 </p>
             </div>
         </div>
