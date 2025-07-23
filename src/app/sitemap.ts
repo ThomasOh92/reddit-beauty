3bc9e6073b9d652ffd1a0d2cfd9183b036e0fab5
Thomas Oh <ohcsthomas@gmail.com>
2025-07-24 00:49:58 +0100

    sitemap update

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -92,6 +92,7 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
     { url: `${APP_URL}/` },
     { url: `${APP_URL}/about` },
     { url: `${APP_URL}/posts` },
+    { url: `${APP_URL}/faq` }
   ];
 
   // Blog posts (Sanity)
