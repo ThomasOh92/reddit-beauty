dbe94eefae6e21d0885a372da146afcd5ebef05b
Thomas Oh <ohcsthomas@gmail.com>
2025-07-31 20:35:26 +0100

    trying to fix redirect

--- middleware.ts
+++ middleware.ts
@@ -3,11 +3,11 @@ import { NextRequest, NextResponse } from 'next/server';
 export function middleware(req: NextRequest) {
   const url = req.nextUrl.clone();
 
-  // Redirect HTTP to HTTPS
-  if (url.protocol === 'http:') {
-    url.protocol = 'https:';
-    return NextResponse.redirect(url);
-  }
+  // // Redirect HTTP to HTTPS
+  // if (url.protocol === 'http:') {
+  //   url.protocol = 'https:';
+  //   return NextResponse.redirect(url);
+  // }
 
   return NextResponse.next();
 }
