98687c3e1efdf5183d3fa2091e707098b74db838
Thomas Oh <ohcsthomas@gmail.com>
2025-08-31 16:15:13 +0100

    next image for pdf guide

--- src/app/pdf-guide/page.tsx
+++ src/app/pdf-guide/page.tsx
@@ -1,5 +1,6 @@
 import ConvertKitEmbed from "@/components/convertkit-embed";
-import { Metadata } from "next"; 
+import { Metadata } from "next";
+import Image from "next/image"; 
   
 export const metadata: Metadata = {
   title: "Reddit-Backed Starter Routine (AM/PM)",
@@ -18,27 +19,39 @@ export default function PdfGuidePage() {
       {/* Carousel */}
       <div className="carousel w-full max-w-[400px]">
         <div id="slide1" className="carousel-item relative w-full">
-          <img
+          <Image
             src="/pdf-cover.png"
-            className="w-full" />
+            className="w-full"
+            alt="PDF Cover"
+            width={400}
+            height={400}
+          />
           <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
             <a href="#slide3" className="btn btn-circle">❮</a>
             <a href="#slide2" className="btn btn-circle">❯</a>
           </div>
         </div>
         <div id="slide2" className="carousel-item relative w-full">
-          <img
+          <Image
             src="/pdf-table-of-contents.png"
-            className="w-full" />
+            className="w-full"
+            alt="PDF Table of Contents"
+            width={400}
+            height={400}
+          />
           <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
             <a href="#slide1" className="btn btn-circle">❮</a>
             <a href="#slide3" className="btn btn-circle">❯</a>
           </div>
         </div>
         <div id="slide3" className="carousel-item relative w-full">
-          <img
+          <Image
             src="/pdf-how-to-use-this-guide.png"
-            className="w-full" />
+            className="w-full"
+            alt="PDF How to Use This Guide"
+            width={400}
+            height={400}
+          />
           <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
             <a href="#slide2" className="btn btn-circle">❮</a>
             <a href="#slide1" className="btn btn-circle">❯</a>
