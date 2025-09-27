93ece24a6a91f2a1f19e2f1d2be460d9257a1564
Thomas Oh <ohcsthomas@gmail.com>
2025-09-27 13:57:41 +0100

    favicon fixes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -37,7 +37,24 @@ export const metadata: Metadata = {
       index: true,
       follow: true,
     }
-  }
+  },
+  icons: {
+    icon: [
+      { url: "/favicon.ico" },
+      { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+      { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+      { url: "/favicon-96x96.png", type: "image/png", sizes: "96x96" },
+      { url: "/favicon.svg", type: "image/svg+xml" },
+    ],
+    shortcut: ["/favicon.ico"],
+    apple: [
+      { url: "/apple-touch-icon.png", sizes: "180x180", type: "image/png" },
+    ],
+  },
+  manifest: "/site.webmanifest",
+  appleWebApp: {
+    title: "T Beauty",
+  },
 };
 
 export default function RootLayout({
